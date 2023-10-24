228e1dfe39383190b8baa8d107f1ec427eac9fdf
juan boglione <boglionejuan33@gmail.com>
2023-10-24 16:22:08 -0300

    primer pusheo y configuracion de todo

---
+++ E-3-Commerce-front/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  tailwindConfig: './styles/tailwind.config.js',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+      }
+    },
+  },
+  plugins: [
+    ['prettier-plugin-tailwindcss'],
+  ],
+}
