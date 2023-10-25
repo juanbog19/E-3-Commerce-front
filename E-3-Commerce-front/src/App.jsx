47a24ea8335d73645afde06de44f3e76da4d78f2
juan boglione <boglionejuan33@gmail.com>
2023-10-25 20:46:03 -0300

    maquetando todo

--- E-3-Commerce-front/src/App.jsx
+++ E-3-Commerce-front/src/App.jsx
@@ -1,21 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Detail from "./components/pages/Detail";
-
+import NavBar from "./components/UI/NavBar";
 
 const App = () => {
 
   return (
     <div>
-
-      <Routes>
-
+      <NavBar />
+    <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/detail/:id' element={<Detail/>} />
-
-
-      </Routes>
-
+    </Routes>
     </div>
   )
 }
--- E-3-Commerce-front/src/components/UI/Card.jsx
+++ E-3-Commerce-front/src/components/UI/Card.jsx
@@ -1,8 +1,8 @@
 import {Link} from 'react-router-dom';
 
 
-const Card = ({cel}) => {
-    const {id, marca, modelo, img, price} = cel
+const Card = ({id, marca, modelo, img, price}) => {
+
     
   return (
     <div>
