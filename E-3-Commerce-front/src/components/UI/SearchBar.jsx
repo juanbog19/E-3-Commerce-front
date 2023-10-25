aede0d63f09c4c22f5cc11c466c1ecf975deef17
KariRomero <karinaromero.1993@gmail.com>
2023-10-24 22:49:44 -0300

    configuracion final de react router dom (rutas) y redux

--- E-3-Commerce-front/src/components/UI/SearchBar.jsx
+++ E-3-Commerce-front/src/components/UI/SearchBar.jsx
@@ -1,8 +1,57 @@
+// import { useState } from "react";
+// import { useDispatch } from "react-redux";
+
+
 const SearchBar = () => {
+
+    // const [celphone, setCelphone] = useState('');
+
+    // const dispatch = useDispatch();
+    // // console.log(celphone);
+
+    // const handleInputChange = (event) => {
+    //     setCelphone(event.target.value);
+    // };
+
+    // const handleOnClick = () => {
+    //     if (!celphone) {
+    //         alert('Por favor digita algo antes de buscar.');
+    //     } else {
+    //         dispatch(getCelphones(celphone));
+    //         setCelphone('');           
+    //     } 
+    // };
+
+    // const handlerReset = () => {
+        
+    //     setCelphone('');
+    //     dispatch(getCelphones())
+    // };
+
+    // const handleKeyPress = (event) => {
+    //     if (event.key === "Enter") {
+    //         // Previene el comportamiento predeterminado del formulario (envío)
+    //         event.preventDefault();
+    //         handleOnClick();
+    //     }
+    // };
+
     return(
         <div>
-
-        </div>
+				<input value={celphone} onChange={handleInputChange} onKeyPress={handleKeyPress} type='search' placeholder='Buscar...' className="px-12 py-2 border-black rounded-lg"/>
+				<button className='px-3 py-2 ml-1 text-gray-100 rounded-lg bg-primary hover:bg-secondary' onClick={handleOnClick}>
+                {/* <Icons icon={faMagnifyingGlass} /> */} Buscar
+				</button>
+                <button className='px-3 py-2 ml-1 text-gray-100 rounded-lg bg-primary hover:bg-secondary' onClick={handlerReset}>
+                {/* <Icons icon={faRotateRight} /> */} Reset
+                </button>
+                
+                {/* <button className='px-8 py-2 ml-1 text-gray-100 rounded-lg bg-primary hover:bg-secondary' onClick={handlerReset}>
+                    <Link to={'/home'}>
+                    Home
+                    </Link>
+                </button> */}
+			</div>
     )
 }
 
