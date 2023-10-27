8e053b9e5a417071f8e44f6362d14cd7113f323b
KariRomero <karinaromero.1993@gmail.com>
2023-10-27 18:57:22 -0300

    Cards vinculadas correctamente a la base de datos

--- E-3-Commerce-front/src/components/UI/Card.jsx
+++ E-3-Commerce-front/src/components/UI/Card.jsx
@@ -1,25 +1,29 @@
-import { Link } from "react-router-dom";
+import {Link} from 'react-router-dom';
 
-const Card = ({ id, marca, modelo, img, price, imageLink, flipImageLink, flipOnHover }) => {
-  const cardStyle = flipOnHover ? "card flip" : "card";
 
+const Card = ({id, image, brand, model, price}) => {
+
+    
   return (
-    <div className={cardStyle}>
-      <div className="card-front">
-        <img src={imageLink} alt={marca} />
-        <h2>{marca}</h2>
-        <p>Modelo: iphone 11{modelo}</p>
-        <p>Price: $399 USD{price}</p>
-        <Link to={`/detail/${id}`}>
-          <button>Details</button>
+    <div>
+        <div>
+            <img src={image} alt={brand} />
+        </div>
+
+        <div>
+            <h2>{brand}</h2>
+        </div>
+
+        <div>
+            <p>Modelo:{model}</p>
+            <p>Price:{price}</p>
+        </div>
+
+        <Link to={`/detail/${id}`} >
+            <button>Details</button>
         </Link>
-      </div>
-      <div className="card-back">
-        <img src={flipImageLink} alt="Image" />
-      </div>
-    </div>
-  );
-};
+</div>
+)
+}
 
 export default Card;
-
--- E-3-Commerce-front/src/components/UI/Cards.jsx
+++ E-3-Commerce-front/src/components/UI/Cards.jsx
@@ -1,23 +1,31 @@
-
 import Card from "./Card";
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { getAllProducts } from "../../redux/store/productsSlice";
 
 
 const Cards = () => {
 
-    const celphones = [    //img = va un Array de Url xq  tiene varios perfiles de celular de frete y de costados etc
-        {id: 1, marca: "LG", modelo:20, img:"https://i5.walmartimages.com/asr/58e60fc7-4cb5-4614-b429-71ef84abdca7.fe632c1d9d4dd994b214456807d3105d.jpeg", price:130},
-        {id: 2, marca: "Xiamoi", modelo:20, img:"https://promart.vteximg.com.br/arquivos/ids/564309-1000-1000/image-a17969df02b54880b56ce5e871b3e4dd.jpg?v=637388265739730000", price:120},
-        {id: 3, marca: "Motorola", modelo:20, img:"https://th.bing.com/th/id/OIP.gEeXCt2ch4YuDY-Rl5ePiQHaHa?pid=ImgDet&rs=1", price:110},
-        {id: 4, marca: "Samsung", modelo:13 , img:"https://th.bing.com/th/id/R.b9b7d8d10efa4ea1dac3701c75ae7fa0?rik=zzJ%2f5Yfvqcfs%2bg&riu=http%3a%2f%2fwww.wired.com%2fwp-content%2fuploads%2f2015%2f08%2fsamsung-story1.jpg&ehk=h7gfKa6tDguTqhVu715IAkgSgv0t%2bW8LYw97%2fIHOA7o%3d&risl=&pid=ImgRaw&r=0", price:780},
-        {id: 4, marca: "iPhone", modelo:13 , img:"https://th.bing.com/th/id/OIP.YQs0gSDQv4dvxEszxD6MVAHaGH?pid=ImgDet&rs=1", price:780},
-    ]
+    const dispatch = useDispatch();
 
+    useEffect(()=>{
+        dispatch(getAllProducts())
+    },[])
+
+    const { products } = useSelector((state)=>state.products)
+    console.log(products);
+
+   
     return (
     <div>
-        {celphones && celphones.map((celphone, index)=>
+        {products && products.map((product)=>
             <Card
-            key={index}
-            celphone={celphone}
+            key={product.id}
+            id={product.id}
+            image={product.image}
+            brand={product.brand.name}
+            model={product.model}
+            price={product.price}
             />
         )}
     </div>
