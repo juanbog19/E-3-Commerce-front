8891dea832989493c0396456ba3481070d5b0e4c
xmontalvvo <xmontalvvo@gmail.com>
2023-10-28 00:06:32 -0500

    Mejoras visuales en las Cards del proyecto

--- E-3-Commerce-front/src/components/UI/Cards.jsx
+++ E-3-Commerce-front/src/components/UI/Cards.jsx
@@ -1,35 +1,36 @@
+// En Cards.js
 import Card from "./Card";
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from "react";
 import { getAllProducts } from "../../store/productsSlice";
 
-
 const Cards = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllProducts());
+  }, []);
 
-    useEffect(()=>{
-        dispatch(getAllProducts())
-    },[])
+  const { products } = useSelector((state) => state.products);
 
-    const { products } = useSelector((state)=>state.products)
-    console.log(products);
-
-   
-    return (
-    <div>
-        {products && products.map((product)=>
-            <Card
+  return (
+    <div className='flex flex-wrap justify-around'>
+      {products &&
+        products.map((product) => (
+          <Card
             key={product.id}
             id={product.id}
             image={product.image}
             brand={product.brand.name}
             model={product.model}
             price={product.price}
-            />
-        )}
+            memory={product.memory}
+            storage={product.storage}
+            size={product.size}
+          />
+        ))}
     </div>
-    )
+  );
 };
 
 export default Cards;
