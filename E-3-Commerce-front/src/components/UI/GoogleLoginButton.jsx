d003542bde11cb8e27f5231c8061697e6329b153
Bladimir G. Parra B. | IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-11-05 02:23:50 -0500

    Update Logion and GoogleLoginButton

--- E-3-Commerce-front/src/components/UI/GoogleLoginButton.jsx
+++ E-3-Commerce-front/src/components/UI/GoogleLoginButton.jsx
@@ -9,33 +9,33 @@ const GoogleLoginButton = () => {
 
   const responseGoogle = async (response) => {
     // Manejar la respuesta de Google
-    const { tokenId } = response;
-    dispatch(loginUser(tokenId))
-      .then((data) => {
-        if (data.token) {
-          dispatch(setToken(data.token));
-          Swal.fire({
-            icon: 'success',
-            title: '¡Inicio de sesión exitoso!',
-            text: '¡Te has autenticado con Google exitosamente!',
-          });
-        } else {
-          dispatch(setError("Error de autenticación"));
-          Swal.fire({
-            icon: 'error',
-            title: 'Error de autenticación',
-            text: 'Hubo un problema al autenticarse con Google',
-          });
-        }
-      })
-      .catch((error) => {
-        dispatch(setError(error.message));
-        Swal.fire({
+    try {
+      const { tokenId } = response;
+      const data = await dispatch(loginUser(tokenId));
+
+      if (data.token) {
+        dispatch(setToken(data.token));
+        await Swal.fire({
+          icon: 'success',
+          title: '¡Inicio de sesión exitoso!',
+          text: '¡Te has autenticado con Google exitosamente!',
+        });
+      } else {
+        dispatch(setError("Error de autenticación"));
+        await Swal.fire({
           icon: 'error',
           title: 'Error de autenticación',
-          text: error.message,
+          text: 'Hubo un problema al autenticarse con Google',
         });
+      }
+    } catch (error) {
+      dispatch(setError(error.message));
+      await Swal.fire({
+        icon: 'error',
+        title: 'Error de autenticación',
+        text: error.message,
       });
+    }
   };
 
   const clientId = "579782132065-5lrtgi0vadgd456rvurshopd7ghu8qju.apps.googleusercontent.com";
