1f34677e221339fe7ab1906d70f9418be0577cb0
KariRomero <karinaromero.1993@gmail.com>
2023-11-08 01:55:13 -0300

    about

--- E-3-Commerce-front/src/components/pages/About.jsx
+++ E-3-Commerce-front/src/components/pages/About.jsx
@@ -1,13 +1,38 @@
-//import './About.css'; // Create a CSS file for styling
-import aboutImage from '../../assets/About.png'; // Import the PNG image
+import iphone from '../../assets/iphone_14pro.png';
 
 const About = () => {
   return (
-    <div className="about-container">
-      <h1>About Us</h1>
-      <div className="image-container">
-        <img src={aboutImage} alt="About Us" />
+    <div className=" flex mt-20 ml-4 mr-4 mb-20 space-x-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:border-gray-300 mt-40 mr-40 ml-40">
+
+      
+      <div>
+
+      <h1 className="flex justify-center text-4xl font-semibold ">PhonePulse</h1>
+
+      <p className="mt-5 mb-5 text-xl ">
+      En PhonePulse, somos apasionados por conectarlos con el pulso de lo mas
+      reciente y destacado en el mundo de los teléfonos celulares. Entendemos 
+      que los consumidores tecnológicamente conscientes de hoy en día buscan
+      tanto caldiad como asequibilidad, y ahi es donde entramos en acción.        
+      </p>
+
+      <h1 className="flex justify-center text-4xl font-semibold ">Nuestra misión</h1>
+
+      <p className="mt-5 mb-5 text-xl ">
+      Nuestra misión es simple pero poderosa: proporcionarles un destino único
+      donde puedan descubrir ofertas increíbles en teléfonos celulares nuevos
+      y reacondicionados. Creemos que todos deberían tener acceso a la tecnología
+      de vanguardia sin quebrantar su bolsillo, y estamos comprometidos en hacerlo
+      realidad.
+      </p>
       </div>
+      
+
+      <div>
+        <img src={iphone} alt='imagen iphone 14 pro' />
+      </div>
+
+
     </div>
   );
 };
