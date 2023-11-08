213ab5c7215c76a1d8f050ef929f6ee120b990c8
Bladimir G. Parra B. | IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-11-08 15:37:36 -0500

    Pagination styles

--- E-3-Commerce-front/src/components/UI/Paginated.jsx
+++ E-3-Commerce-front/src/components/UI/Paginated.jsx
@@ -1,34 +1,36 @@
-const Paginated = ({ productsPerPage, allProducts, paginado, currentPage}) => {
+const Paginated = ({ productsPerPage, allProducts, paginado, currentPage }) => {
+  const pageNumber = [];
+  const cantPaginas = Math.ceil(allProducts / productsPerPage);
 
-    const pageNumber = []
-    const cantPaginas = Math.ceil(allProducts / productsPerPage)
+  for (let i = 1; i <= cantPaginas; i++) {
+    //el Math.ceil() devuelve el entero mayor o igual más próximo a un número dado.
+    //i <= 100/15 = 6.66 => Math.ceil(6.66) = 7 => 1 <= 7
+    pageNumber.push(i);
+  }
 
-    for (let i = 1; i <= cantPaginas; i++) {   //el Math.ceil() devuelve el entero mayor o igual más próximo a un número dado.
-        //i <= 100/15 = 6.66 => Math.ceil(6.66) = 7 => 1 <= 7
-        pageNumber.push(i)
-    }
+  return (
+    <nav className="w-full h-12 flex justify-center">
+      <div className="flex space-x-1 items-center bg-white rounded-full p-2">
+        {pageNumber &&
+          pageNumber.map((number) => (
+            <span key={number}>
+              <button
+                onClick={() => paginado(number)}
+                className={`${
+                  number === currentPage
+                    ? "bg-violet-300 text-white"
+                    : "bg-white text-black"
+                } px-3 py-1 rounded-full`}
+                disabled={number === currentPage}
+              >
+                {number}
+              </button>
+              {/* y por cada elemento renderizame un boton y agregales un evento onClick, el cual establecera el numero de pagina en el que me encuentro*/}
+            </span>
+          ))}
+      </div>
+    </nav>
+  );
+};
 
-
-    return (
-        <nav className='w-full h-12 '>
-            <div className='space-x-1 text-center bg-white rounded-full '>
-
-                {pageNumber && pageNumber.map(number => ( //si en pageNumber hay algo mapealo
-                    <span key={number}>
-                        <button
-                            onClick={() => paginado(number)}
-                            className={number === currentPage ? 'bg-violet-300' : ''}
-                            disabled={number === currentPage}
-                        >
-                            {number}
-                        </button> {/* y por cada elemento renderizame un boton y agregales un evento onClick, el cual establecera el numero de pagina en el que me encuentro*/}
-                    </span>
-                ))}
-
-
-            </div>
-        </nav>
-    )
-}
-
-export default Paginated
+export default Paginated;
