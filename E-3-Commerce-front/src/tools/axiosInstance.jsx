48f23d4c7c503fc3a648df667d403256b2ffb473
EmiCastro1997 <casacastro97@gmail.com>
2023-11-11 19:46:06 -0300

    estilos de reviews con alerts

--- E-3-Commerce-front/src/tools/axiosInstance.jsx
+++ E-3-Commerce-front/src/tools/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   //baseURL: "https://e-3-commerce-back-production.up.railway.app/",
-  baseURL: "http://localhost:3001",
+ baseURL: "http://localhost:3001",
 });
 
 export default instance;
