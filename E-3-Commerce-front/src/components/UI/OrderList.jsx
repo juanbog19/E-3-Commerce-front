ac9d0f7e2804a985926ffca747ba3d8923be3534
juan boglione <boglionejuan33@gmail.com>
2023-11-10 23:34:27 -0300

    cambios de las ordenes

--- E-3-Commerce-front/src/components/UI/OrderList.jsx
+++ E-3-Commerce-front/src/components/UI/OrderList.jsx
@@ -1,24 +1,36 @@
-// import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import axiosURL from '../../tools/axiosInstance';
+import { useState } from "react";
+
+const OrderList = () => {
+
+    const [order, setOrder] = useState([]);
+
+    useEffect(() => {
+      axiosURL.get("/orders/")
+        .then((response) => {
+          setOrder(response.data); 
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }, []);
 
 
-// const OrderList = () => {
-    
-//     const ordering = useSelector((state) => state.user.orders);
+  return (
+    <li className="py-2 mt-20 border-b">
+      <h4 className="text-lg font-bold">Order #{order.id}</h4>
+      <p className="text-gray-600">Date: {order.date}</p>
+      <ul>
+        {order.map((item) => (
+          <li key={item.id} className="flex justify-between">
+            <span>{item.productName}</span>
+            <span>{item.quantity} x ${item.price}</span>
+          </li>
+        ))}
+      </ul>
+    </li>
+  );
+};
 
-//     return (
-//       <li className="border-b py-2">
-//         <h4 className="text-lg font-bold">Order #{ordering.id}</h4>
-//         <p className="text-gray-600">Date: {ordering.date}</p>
-//         <ul>
-//           {ordering.map((item) => (
-//             <li key={item.id} className="flex justify-between">
-//               <span>{item.productName}</span>
-//               <span>{item.quantity} x ${item.price}</span>
-//             </li>
-//           ))}
-//         </ul>
-//       </li>
-//     );
-//   };
-  
-//   export default OrderList;
+export default OrderList;
