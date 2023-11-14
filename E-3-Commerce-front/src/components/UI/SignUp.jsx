4d84c0c1c833098986056470a9ff923d995cf313
xmontalvvo <xmontalvvo@gmail.com>
2023-11-14 14:27:33 -0500

    Correccion de bugs en registro de usuario

--- E-3-Commerce-front/src/components/UI/SignUp.jsx
+++ E-3-Commerce-front/src/components/UI/SignUp.jsx
@@ -76,6 +76,10 @@ const SignUp = () => {
                   value: true,
                   message: "El usuario es obligatorio.",
                 },
+                pattern: {
+                  value: /^(?!\s*$).+/i,
+                  message: "EL usuario debe ser un usuario valido.",
+                },
               })}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-300 dark:placeholder-gray-400 dark:text-black"
               placeholder="Ingresa tu usuario"
