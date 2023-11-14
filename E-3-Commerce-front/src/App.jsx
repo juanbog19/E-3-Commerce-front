24890671a56c71ee0b86beb428999d114f3d9e93
OmarCarrillo1121 <omarcarrillo1121@gmail.com>
2023-11-14 10:41:20 -0600

    Añadida pestaña reviews al dashboard

--- E-3-Commerce-front/src/App.jsx
+++ E-3-Commerce-front/src/App.jsx
@@ -2,8 +2,8 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Detail from "./components/pages/Detail";
 import NavBar from "./components/UI/NavBar";
-import About from "./components/pages/About"
-import ContactUs from "./components/pages/ContactUs"
+import About from "./components/pages/About";
+import ContactUs from "./components/pages/ContactUs";
 import Dashboard from "./components/dashboard/Dashboard";
 import Brands from "./components/dashboard/Brands";
 import Products from "./components/dashboard/Products";
@@ -22,47 +22,57 @@ import OrderList from "./components/UI/OrderList";
 import { useSelector } from "react-redux";
 import NotAllowed from "./components/dashboard/NotAllowed";
 import NotFound from "./components/pages/NotFound";
+import Reviews from "./components/dashboard/Reviews";
 
 const App = () => {
-  
   const location = useLocation();
   const isDashboardRoute = location.pathname.startsWith("/admin");
-  const isAdmin = useSelector(state => state.user.user?.rol)
-  const isLoggedin = useSelector(state => state.user?.loggedin)
+  const isAdmin = useSelector((state) => state.user.user?.rol);
+  const isLoggedin = useSelector((state) => state.user?.loggedin);
 
   return (
     <div>
       {!isDashboardRoute && <NavBar />}
       <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/tienda' element={<Home />} />
-        <Route path='/myProfile' element={<UserProfile />} />
-        <Route path='/orders' element={<OrderList />} />
-        <Route path='/detail/:id' element={<Detail />} />
-        <Route path='/login' element={isLoggedin ? <NotFound/> :<Login /> } />
-        <Route path='/signup' element={isLoggedin ? <NotFound/> : <SignUp />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contacto' element={<ContactUs />} />
-        <Route path='/checkout' element={<Checkout />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/tienda" element={<Home />} />
+        <Route path="/myProfile" element={<UserProfile />} />
+        <Route path="/orders" element={<OrderList />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/login" element={isLoggedin ? <NotFound /> : <Login />} />
+        <Route
+          path="/signup"
+          element={isLoggedin ? <NotFound /> : <SignUp />}
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacto" element={<ContactUs />} />
+        <Route path="/checkout" element={<Checkout />} />
         {/* Rutas administrativas */}
         {isAdmin ? (
           <>
-            <Route path='/admin' element={<Dashboard />} />
-            <Route path='/admin/brands' element={<Brands />} />
-            <Route path='/admin/brands/create' element={<BrandsCreateForm />} />
-            <Route path='/admin/brands/edit/:id' element={<BrandsEditForm />} />
-            <Route path='/admin/products' element={<Products />} />
-            <Route path='/admin/products/create' element={<ProductsCreateForm />} />
-            <Route path='/admin/products/edit/:id' element={<ProductsEditForm />} />
-            <Route path='/admin/users' element={<Users />} />
-            <Route path='/admin/orders' element={<Orders />} />
+            <Route path="/admin" element={<Dashboard />} />
+            <Route path="/admin/brands" element={<Brands />} />
+            <Route path="/admin/brands/create" element={<BrandsCreateForm />} />
+            <Route path="/admin/brands/edit/:id" element={<BrandsEditForm />} />
+            <Route path="/admin/products" element={<Products />} />
+            <Route
+              path="/admin/products/create"
+              element={<ProductsCreateForm />}
+            />
+            <Route
+              path="/admin/products/edit/:id"
+              element={<ProductsEditForm />}
+            />
+            <Route path="/admin/users" element={<Users />} />
+            <Route path="/admin/orders" element={<Orders />} />
+            <Route path="/admin/reviews" element={<Reviews />} />
           </>
         ) : (
-          <Route path='/admin/*' element={<NotAllowed />} />
+          <Route path="/admin/*" element={<NotAllowed />} />
         )}
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
