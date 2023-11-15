d1d9307421ffde18ca00a6782c5cca54779c38b7
xmontalvvo <xmontalvvo@gmail.com>
2023-11-15 11:33:58 -0500

    Validacion de errores en envio de review

--- E-3-Commerce-front/src/components/UI/Reviews.jsx
+++ E-3-Commerce-front/src/components/UI/Reviews.jsx
@@ -4,6 +4,7 @@ import { getReviews, postReview } from '../../store/reviewSlice';
 import Swal from 'sweetalert2';
 import { RatingInput } from './RatingInput';
 import { FaStar } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
 
 const Review = () => {
   const dispatch = useDispatch();
@@ -17,6 +18,7 @@ const Review = () => {
   console.log(':::::::::', validateOrders.length >= 1)
 
   //console.log('esto es reviews:',reviews);
+  const { register, handleSubmit, getValues, setValue, formState: { errors } } = useForm();
 
   const [newReviewData, setNewReviewData] = useState({
     comment: '',
@@ -31,24 +33,37 @@ const Review = () => {
     });
   };
 
-  const handlePostReview = useCallback(async () => {
-    // if (!hasPurchased) {
-    //   Swal.fire('Debes realizar una compra antes de dejar tu experiencia', 'Click para continuar', 'error');
-    //   return;
-    // }
+  // const handlePostReview = useCallback(async () => {
+  //   try {
+  //     await dispatch(postReview(newReviewData));
+  //     dispatch(getReviews());
+  //     setNewReviewData({
+  //       comment: '',
+  //       rating: 0,
+  //       id_user: loggedInUser.id,
+  //     });      
+  //   } catch (error) {
+  //     Swal.fire('Error al agregar una nueva revisión:', "Click para continuar", "info", error.message);
+  //   }
+  // }, [dispatch, newReviewData]);
 
+  const onSubmit = async (data) => {
+    console.log('ESTO ES DATA',data)
+    //console.log(loggedInUser)
+    data.id_user = users.id;
     try {
-      await dispatch(postReview(newReviewData));
+      await dispatch(postReview(data))
       dispatch(getReviews());
-      setNewReviewData({
-        comment: '',
-        rating: 0,
-        id_user: loggedInUser.id,
-      });      
+
+      setValue('comment', '')
+      setValue('rating', '')
+      setValue('id_user', '')
+
+      console.log('ESTO ES DATA VACIADO', data)
     } catch (error) {
-      Swal.fire('Error al agregar una nueva revisión:', "Click para continuar", "info", error.message);
+      console.log(error)
     }
-  }, [dispatch, newReviewData]);
+  }
 
   const formatDateString = (dateString) => {
     const date = new Date(dateString);
@@ -59,35 +74,42 @@ const Review = () => {
     return `${day}/${month}/${year}`;
   };
 
+  //console.log(newReviewData)
   return (
     <div className="flex flex-col justify-center items-center mt-20 border-black">
       {validateOrders.length >= 1 ? (
         <div className="w-1/3 p-4 border-gray-200 rounded-lg shadow mb-5">
+          <form onSubmit={handleSubmit(onSubmit)}>
           <label className="block mb-2 text-lg font-bold">
             Coméntanos tu experiencia:
             <input
               type="text"
               name="comment"
+              {...register('comment', { required: 'Este campo es obligatorio.' })}
               value={newReviewData.comment}
               onChange={handleInputChange}
               placeholder='Escribe un comentario sobre tu experiencia...'
               className="border p-2 w-full mb-4 focus:outline-none focus:border-purple-500"
-              required
             />
+            {errors.comment && <p className="text-red-500 text-xs">{errors.comment.message}</p>}
           </label>
-          <RatingInput rating={newReviewData.rating} handleInputChange={handleInputChange} />
+          <RatingInput
+              rating={newReviewData.rating}
+              onChange={(value) => setValue('rating', value)} // Establecer el valor del rating usando setValue
+            />
+            {errors.rating && <p className="text-red-500 text-xs">{errors.rating.message}</p>}
           <p>Realiza una compra para poder dar una review</p>
           {!newReviewData.comment && (
             <p className="text-purple-500 text-lg mt-6 animate-pulse">
               ¡Ayúdanos a mejorar tu experiencia dejándonos tu comentario!
             </p>
           )}
-          <button
+          <button type='submit'
             className="w-full text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
-            onClick={handlePostReview}
           >
             Compartir
           </button>
+          </form>
         </div>
       ) : (
         ''
