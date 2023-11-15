dd9d78718e24d42733147e1c7bbc1856c56c829c
KariRomero <karinaromero.1993@gmail.com>
2023-11-15 16:14:02 -0300

    se quito button en thank you page, se arregló bug de ordenes en UserProfile/OrderList

--- E-3-Commerce-front/src/components/UI/OrderList.jsx
+++ E-3-Commerce-front/src/components/UI/OrderList.jsx
@@ -1,31 +1,18 @@
-const OrderList = ({ id, date, brand, model, amount, order }) => {
+const OrderList = ({ id, brand, model, amount }) => {
 
   return (
-    <li >
-      <ul>
-        <li key={id} className="flex items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:border-gray-300">
-          <div className="flex flex-col">
-            <h1 className="mb-2 text-lg font-bold">Orden #{order}</h1>
-            <h4 className="text-sm font-semibold text-gray-600">N° de órden: {id}</h4>
-            <p className="text-xs text-gray-500">Fecha de compra: {date}</p>
-          </div>
-          <div className="flex flex-col items-end">
-            {(brand.length > 0 && brand.length === model.length) ? (
-              brand.map((_, i) => (
-              <div key={i} className="text-sm">
-                {brand[i]} - {model[i]}
-              </div>
-            ))
-            ) : (
-            <div className="text-sm">
-              {brand.length > 0 && `${brand[0]} - ${model[0]}`}
-            </div>
-          )}
-            <span className="mt-2 text-lg font-bold">${amount}</span>
-          </div>
-        </li>
-      </ul>
-    </li>
+    <div className="flex">
+      <div className="mr-80 text-m">            
+          <h1>Producto: {brand} {model}</h1>           
+      </div>
+
+      <div className="ml-80 text-lg font-bold">            
+          <span >${amount}</span>
+      </div>
+    </div>
+    
+          
+        
   );
 };
 
--- E-3-Commerce-front/src/components/pages/UserProfile.jsx
+++ E-3-Commerce-front/src/components/pages/UserProfile.jsx
@@ -4,51 +4,109 @@ import { useEffect, useState } from 'react';
 import { getAllOrders, getOrderById } from '../../store/orderSlice';
 import OrderList from '../UI/OrderList';
 
+// const UserProfile = () => {
+//   const dispatch = useDispatch();
+//   const user = useSelector((state) => state.user?.user);
+//   const productsById = useSelector(state => state.orders.orders)
+//   const userId = user?.id;
+//   console.log(productsById);
+
+//   useEffect(() => {
+//     dispatch(getOrderById(userId))
+
+//   }, []);
+
+//   return (
+//     <div className="max-w-4xl mx-auto my-10 mt-20 border-collapse">
+//       <h1 className="mb-6 text-3xl font-bold text-center uppercase">
+//         Perfil del Usuario
+//       </h1>
+//       <div className="p-4 mb-6 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:border-gray-300">
+//         <h2 className="mb-2 text-xl font-semibold">
+//           Nombre: {user ? user.username : ''}
+//         </h2>
+//         <h2 className="text-xl font-semibold">
+//           Email: {user ? user.email : ''}
+//         </h2>
+//       </div>
+//       <div className="overflow-hidden border rounded-md">
+//         <h3 className="px-4 py-3 text-xl font-bold text-gray-700 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:border-gray-300">
+//           Historial de Órdenes
+//         </h3>
+//         <ul>
+//           {productsById.map((order, index) => (
+//             <OrderList
+//               key={index}
+//               id={order.id}
+//               date={order.date}
+//               brand={order.products.brand ? order.products.brand.name : 'no se encontró marca'}
+//               model={order.products ? order.products.model : 'no se encontró modelo'}
+//               amount={order.amount}
+//               order={order.order}
+//             />
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+
+// export default UserProfile;
+
+
 const UserProfile = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user?.user);
-  const productsById = useSelector(state => state.orders.orders)
+  const orders = useSelector((state) => state.orders.orders); // Updated variable name
   const userId = user?.id;
 
   useEffect(() => {
-    dispatch(getOrderById(userId))
-
-  }, []);
+    dispatch(getOrderById(userId));
+  }, [dispatch, userId]);
 
   return (
     <div className="max-w-4xl mx-auto my-10 mt-20 border-collapse">
+      
       <h1 className="mb-6 text-3xl font-bold text-center uppercase">
-        Perfil del Usuario
-      </h1>
-      <div className="p-4 mb-6 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:border-gray-300">
-        <h2 className="mb-2 text-xl font-semibold">
-          Nombre: {user ? user.username : ''}
-        </h2>
-        <h2 className="text-xl font-semibold">
-          Email: {user ? user.email : ''}
-        </h2>
-      </div>
-      <div className="overflow-hidden border rounded-md">
-        <h3 className="px-4 py-3 text-xl font-bold text-gray-700 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:border-gray-300">
-          Historial de Órdenes
-        </h3>
-        <ul>
-          {productsById.map((order, index) => (
-            <OrderList
-              key={index}
-              id={order.id}
-              date={order.date}
-              brand={order.products.map(product => product.brand.name)}
-              model={order.products.map(product => product.model)}
-              amount={order.amount}
-              order={order.order}
-            />
-          ))}
-        </ul>
-      </div>
+         Perfil del Usuario
+       </h1>
+       <div className="p-4 mb-6 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:border-gray-300">
+         <h2 className="mb-2 text-xl font-semibold">
+           Nombre: {user ? user.username : ''}
+         </h2>
+         <h2 className="text-xl font-semibold">
+           Email: {user ? user.email : ''}
+         </h2>
+       </div>
+       <div className="overflow-hidden border rounded-md">
+         <h3 className="px-4 py-3 text-xl font-bold text-gray-700 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:border-gray-300">
+           Historial de Órdenes
+         </h3>
+</div>
+      <ul className="flex flex-col mb-5 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:border-gray-300"> 
+        {orders.map((order) => (
+          <div key={order.id}>
+            <h3 className="text-lg font-semibold">Orden ID: {order.id}</h3>
+            <p>Fecha: {order.date}</p>            
+            {order.products && order.products.length > 0 ? (
+              order.products.map((product) => (
+                <OrderList
+                  key={product.id}
+                  id={product.id}                 
+                  brand={product.brand ? product.brand.name : 'No se encontró marca'}
+                  model={product.model ? product.model : 'No se encontró modelo'}
+                  amount={order.amount}                  
+                />
+              ))
+            ) : (
+              <p>No hay productos en esta orden.</p>
+            )}
+          </div>
+        ))}
+      </ul>
     </div>
   );
 };
-
 
 export default UserProfile;
